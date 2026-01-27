537a9988614ea06a866329fd9903379ddb2afe42
abdulakeem-asiru <asiruabdulakeem@gmail.com>
2026-01-27 08:01:18 +0100

    Dashboard Layout

--- components/dashboard/Sidebar.tsx
+++ components/dashboard/Sidebar.tsx
@@ -7,17 +7,17 @@ import { Favicon, Logo } from '@/app/assest/images'
 
 
 const Sidebar = () => {
-  const [collapsed, setCollapsed] = useState(false);
+  const [collapsed, setCollapsed] = useState(true);
   return (
     <aside className={`hidden px-4 py-8 md:block shrink-0 "
     ${collapsed ? "w-85" : "w-30"}`}>
     <div className=" pb-10 flex flex-col gap-8 ">
       <div className='flex justify-between items-center'>
       <Image src={collapsed ? Logo : Favicon}
-  width={collapsed ? 120 : 40}
-  height={collapsed ? 120 : 40}
-  alt="Logo"
-  className="ml-4" />
+        width={collapsed ? 120 : 40}
+        height={collapsed ? 120 : 40}
+        alt="Logo"
+        className="ml-4" />
 
         {
           collapsed ? 
